fa18cd0b2149f3c7939153b1232780fb365c331b
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-07-08 18:36:23 +0200

    feat: initialized shell for backend spring boot application.

---
+++ generator_output/backend-service/src/main/java/ftn/backendservice/exception/exceptions/AuthorizationException.java
@@ -0,0 +1,13 @@
+package ftn.backendservice.exception.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class AuthorizationException extends RuntimeException {
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+
+}
