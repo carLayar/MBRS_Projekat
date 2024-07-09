defa88ff42b9d98cd5e810eae62577f0d904b648
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-07-09 20:17:19 +0200

    feat: added generation of repository classes.

--- generator/generator/generator.py
+++ generator/generator/generator.py
@@ -2,6 +2,7 @@ import os
 
 from generator.config.generator_options import GeneratorOptions
 from generator.model_generator import ModelGenerator
+from generator.repository_generator import RepositoryGenerator
 
 
 class Generator:
@@ -11,11 +12,17 @@ class Generator:
 
     def generate(self):
         self.generate_model()
+        self.generate_repository_layer()
 
     def generate_model(self):
         generator_options = GeneratorOptions("generator_output\\backend-service\\src\\main\\java".replace("\\", os.sep), "jpatemplate.jinja", "./templates", "{0}.java", "ftn.backendservice.domain.entities")
         model_generator = ModelGenerator(generator_options, self.classes)
         model_generator.generate()
 
-
+    def generate_repository_layer(self):
+        generator_options = GeneratorOptions("generator_output\\backend-service\\src\\main\\java".replace("\\", os.sep),
+                                             "repository.jinja", "./templates", "{0}Repository.java",
+                                             "ftn.backendservice.repositories")
+        repository_generator = RepositoryGenerator(generator_options, self.classes)
+        repository_generator.generate()
 
